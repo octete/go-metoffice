145a15c6dbe5387b2521badfa4ab46de4ce9fc7c
Jesús Roncero <jesus.roncero@shazam.com>
2017-11-19 22:52:33 +0000

    Test GetMountainForecasts

--- mountainareaforecast_test.go
+++ mountainareaforecast_test.go
@@ -1,1 +1,19 @@
 package metoffice
+
+import "testing"
+
+func TestClient_GetMountainForecast(t *testing.T) {
+	t.Parallel()
+
+	var err error
+
+	// Get
+	var mfItems []MountainForecastItem
+	record(t, "mountainforecastlist/get", func(c *Client) {
+		mfItems, err = c.GetMountainAreaCapabilities()
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
